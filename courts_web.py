2fa2316f7229b7d134811117edfaea4012734f5a
hug0sama <160454283+hug0sama@users.noreply.github.com>
2024-03-17 20:12:02 +0800

    Update courts_web.py

--- courts_web.py
+++ courts_web.py
@@ -14,7 +14,7 @@ end = f"{today.strftime('%Y-%m-%d')}T15:59:00.000Z"
 yest = datetime.today() - timedelta(1)
 start = f"{yest.strftime('%Y-%m-%d')}T16:00:00.000Z"
 
-when = st.text_input("pm or tmr or today:")
+when = st.text_input("pm/tmr/today:")
 if when == "pm":
 	pattern2 = r'\b\d{1,2}:\d{2} PM\b'
 elif when == "today":
