42e3ed8a074ef1f6abf6c71e132fe96573325596
hug0sama <160454283+hug0sama@users.noreply.github.com>
2024-04-05 12:46:06 +0800

    Update courts_web.py

--- courts_web.py
+++ courts_web.py
@@ -85,7 +85,7 @@ if st.button("Generate"):
 				result.append(temp)
 			#st.write(court, ",".join(result))
 			st.write(court)
-			for i in result:
+			for i in range(size):
 				st.write(result[i])
 			count += 1
 			size = 0
